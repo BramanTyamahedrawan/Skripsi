c5f4a0ef28785ba97f890981a556d4d445ea7b09
braman tyamahendrawan <brammahendrawan693@gmail.com>
2025-04-11 21:55:43 +0700

    refactor mapel
    Signed-off-by: braman tyamahendrawan <brammahendrawan693@gmail.com>

--- frontend/src/views/subject/forms/edit-mapel-form.jsx
+++ frontend/src/views/subject/forms/edit-mapel-form.jsx
@@ -1,8 +1,24 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React from "react";
-import { Form, Input, Modal } from "antd";
+import React, { useState, useEffect } from "react";
+import {
+  Form,
+  Input,
+  Modal,
+  Select,
+  Table,
+  Tabs,
+  Row,
+  Col,
+  message,
+} from "antd";
+import { getMapel } from "@/api/mapel";
+import { getSchool } from "@/api/school";
+import { reqUserInfo } from "@/api/user";
+
 const { TextArea } = Input;
+const { Option } = Select;
+const { TabPane } = Tabs;
 
 const EditSubjectForm = ({
   visible,
@@ -12,73 +28,124 @@ const EditSubjectForm = ({
   currentRowData,
 }) => {
   const [form] = Form.useForm();
-  const { id, name, description } = currentRowData;
+  const [mapel, setMapel] = useState([]);
 
-  const formItemLayout = {
-    labelCol: {
-      xs: { span: 24 },
-      sm: { span: 8 },
-    },
-    wrapperCol: {
-      xs: { span: 24 },
-      sm: { span: 16 },
-    },
+  const [tableLoading, setTableLoading] = useState(false);
+  const [userSchoolId, setUserSchoolId] = useState([]); // State untuk menyimpan ID sekolah user
+  const [schoolList, setSchoolList] = useState([]);
+
+  const fetchUserInfo = async () => {
+    try {
+      const response = await reqUserInfo(); // Ambil data user dari API
+      setUserSchoolId(response.data.school_id); // Simpan ID sekolah user ke state
+      console.log("User School ID: ", response.data.school_id);
+    } catch (error) {
+      message.error("Gagal mengambil informasi pengguna");
+    }
   };
 
-  const handleSubmit = () => {
-    form
-      .validateFields()
-      .then((values) => {
-        onOk(values);
-      })
-      .catch((info) => {
-        console.log("Validate Failed:", info);
+  const fetchSchoolList = async () => {
+    try {
+      const result = await getSchool();
+      if (result.data.statusCode === 200) {
+        setSchoolList(result.data.content);
+      } else {
+        message.error("Gagal mengambil data");
+      }
+    } catch (error) {
+      message.error("Terjadi kesalahan: " + error.message);
+    }
+  };
+
+  const fetchMapel = async () => {
+    setTableLoading(true);
+    try {
+      const result = await getMapel();
+      if (result.data.statusCode === 200) {
+        setMapel(result.data.content);
+      } else {
+        message.error("Gagal mengambil data");
+      }
+    } catch (error) {
+      message.error("Terjadi kesalahan: " + error.message);
+    } finally {
+      setTableLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserInfo();
+    fetchSchoolList();
+    fetchMapel();
+
+    if (currentRowData) {
+      form.setFieldsValue({
+        idMapel: currentRowData.idMapel,
+        idSchool: currentRowData.school?.idSchool,
+        name: currentRowData.name,
       });
+    }
+  }, [currentRowData, form]);
+
+  useEffect(() => {
+    if (userSchoolId) {
+      form.setFieldsValue({ idSchool: userSchoolId });
+    }
+  }, [userSchoolId, form]);
+
+  const handleSubmit = async () => {
+    try {
+      const values = await form.validateFields();
+      onOk(values);
+    } catch (error) {
+      console.error("Validation failed:", error);
+    }
   };
 
   return (
     <Modal
       title="Edit Mata Kuliah"
-      visible={visible}
-      onCancel={onCancel}
+      open={visible}
+      onCancel={() => {
+        form.resetFields();
+        onCancel();
+      }}
       onOk={handleSubmit}
       confirmLoading={confirmLoading}
+      okText="Simpan"
+      width={1000}
     >
-      <Form
-        {...formItemLayout}
-        form={form}
-        initialValues={{
-          id,
-          name,
-          description,
-        }}
-      >
-        <Form.Item label="ID Mata Kuliah:" name="id">
-          <Input disabled />
-        </Form.Item>
-
-        <Form.Item
-          label="Nama Mata Kuliah:"
-          name="name"
-          rules={[
-            { required: true, message: "Silahkan isikan nama mata kuliah" },
-          ]}
-        >
-          <Input placeholder="Nama Mata Kuliah" />
-        </Form.Item>
-
-        <Form.Item
-          label="Deskripsi Mata Kuliah:"
-          name="description"
-          rules={[
-            {
-              required: true,
-              message: "Silahkan isikan deskripsi mata kuliah",
-            },
-          ]}
-        >
-          <TextArea rows={4} placeholder="Deskripsi Mata Kuliah" />
-        </Form.Item>
+      <Form form={form} layout="vertical">
+        <Row gutter={16}>
+          <Col xs={24} sm={24} md={12}>
+            <Form.Item
+              label="Sekolah:"
+              name="idSchool"
+              rules={[{ required: true, message: "Silahkan pilih Kelas" }]}
+            >
+              <Select defaultValue={userSchoolId} disabled>
+                {schoolList
+                  .filter(({ idSchool }) => idSchool === userSchoolId) // Hanya menampilkan sekolah user
+                  .map(({ idSchool, nameSchool }) => (
+                    <Option key={idSchool} value={idSchool}>
+                      {nameSchool}
+                    </Option>
+                  ))}
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col xs={24} sm={24} md={12}>
+            <Form.Item
+              label="Nama Mata Pelajaran:"
+              name="name"
+              rules={[
+                { required: true, message: "Silahkan isikan Mata Pelajaran" },
+              ]}
+            >
+              <Input placeholder="Mata Pelajaran" />
+            </Form.Item>
+          </Col>
+        </Row>
       </Form>
     </Modal>
   );
--- frontend/src/views/subject/index.jsx
+++ frontend/src/views/subject/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import {
   Card,
   Button,
@@ -10,17 +10,24 @@ import {
   Col,
   Upload,
   Divider,
+  Input,
+  Space,
 } from "antd";
 import {
   EditOutlined,
   DeleteOutlined,
   UploadOutlined,
   PlusOutlined,
+  SearchOutlined,
 } from "@ant-design/icons";
-import { getMapel, deleteMapel, addMapel } from "@/api/mapel";
+import { getMapel, deleteMapel, addMapel, editMapel } from "@/api/mapel";
 import TypingCard from "@/components/TypingCard";
 import AddMapelForm from "./forms/add-mapel-form";
 import EditMapelForm from "./forms/edit-mapel-form";
+import { Skeleton } from "antd";
+import Highlighter from "react-highlight-words";
+import { reqUserInfo, getUserById } from "@/api/user";
+import { render } from "less";
 
 const Mapel = () => {
   const [mapel, setMapel] = useState([]);
@@ -28,31 +35,76 @@ const Mapel = () => {
   const [addMapelModalLoading, setAddMapelModalLoading] = useState(false);
   const [editMapelModalVisible, setEditMapelModalVisible] = useState(false);
   const [editMapelModalLoading, setEditMapelModalLoading] = useState(false);
+  const [importModalVisible, setImportModalVisible] = useState(false);
+  const [uploading, setUploading] = useState(false);
   const [currentRowData, setCurrentRowData] = useState({});
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [userIdJson, setUserIdJson] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const searchInput = useRef(null);
+
+  const editMapelFormRef = useRef();
+  const addMapelFormRef = useRef();
 
   useEffect(() => {
-    fetchMapel();
+    const initializeData = async () => {
+      const userInfoResponse = await reqUserInfo();
+      const { id: userId } = userInfoResponse.data;
+
+      await getUserInfoJson(userId);
+    };
+
+    initializeData();
   }, []);
 
-  const fetchMapel = async () => {
+  const fetchMapel = useCallback(async () => {
+    setLoading(true);
     try {
       const result = await getMapel();
-      if (
-        result.data.statusCode === 200 &&
-        Array.isArray(result.data.content)
-      ) {
-        setMapel(result.data.content);
+      const { content, statusCode } = result.data;
+      if (statusCode === 200) {
+        const filteredContent = content.filter(
+          (item) => item.school?.idSchool === userIdJson
+        );
+        setMapel(filteredContent);
       } else {
-        setMapel([]);
         message.error("Gagal mengambil data");
       }
     } catch (error) {
-      setMapel([]);
       message.error("Terjadi kesalahan: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, [userIdJson]);
+
+  useEffect(() => {
+    if (userIdJson) {
+      fetchMapel();
+    }
+  }, [userIdJson, fetchMapel]);
+
+  const filteredData = mapel.filter((item) => {
+    const query = searchQuery.toLowerCase();
+    return (
+      (item?.name?.toLowerCase() || "").includes(query) ||
+      (item?.nameSchool?.toLowerCase() || "").includes(query)
+    );
+  });
+
+  const getUserInfoJson = async (userId) => {
+    const result = await getUserById(userId);
+    const { content, statusCode } = result.data;
+    if (statusCode === 200) {
+      setUserIdJson(content[0].school.idSchool); // Ubah dari userId ke schoolId
     }
   };
 
   const handleDeleteMapel = (row) => {
+    const { idMapel } = row;
     Modal.confirm({
       title: "Konfirmasi",
       content: "Apakah Anda yakin ingin menghapus data ini?",
@@ -61,7 +113,7 @@ const Mapel = () => {
       cancelText: "Tidak",
       onOk: async () => {
         try {
-          await deleteMapel({ id: row.id });
+          await deleteMapel({ idMapel });
           message.success("Berhasil dihapus");
           fetchMapel();
         } catch (error) {
@@ -83,14 +135,41 @@ const Mapel = () => {
   const handleAddMapelOk = async (values) => {
     setAddMapelModalLoading(true);
     try {
-      await addMapel(values);
+      const updatedData = {
+        idMapel: null,
+        name: values.name,
+        idSekolah: values.idSchool,
+      };
+      await addMapel(updatedData);
+      setAddMapelModalVisible(false);
       message.success("Berhasil menambahkan");
       fetchMapel();
-      setAddMapelModalVisible(false);
     } catch (error) {
+      setAddMapelModalVisible(false);
       message.error("Gagal menambahkan: " + error.message);
     } finally {
       setAddMapelModalLoading(false);
+    }
+  };
+
+  const handleEditMapelOk = async (values) => {
+    setEditMapelModalLoading(true);
+    try {
+      const updatedData = {
+        idMapel: values.idMapel,
+        name: values.name,
+        idSekolah: values.idSchool,
+      };
+      await editMapel(updatedData, currentRowData.idMapel);
+      setEditMapelModalVisible(false);
+      setEditMapelModalLoading(false);
+      message.success("Berhasil mengedit");
+      fetchMapel();
+    } catch (error) {
+      setEditMapelModalVisible(false);
+      message.error("Gagal mengedit: " + error.message);
+    } finally {
+      setEditMapelModalLoading(false);
     }
   };
 
@@ -99,18 +178,136 @@ const Mapel = () => {
     setEditMapelModalVisible(false);
   };
 
+  const handleSearchTable = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+
+  const handleSearch = (keyword) => {
+    setSearchKeyword(keyword);
+    getMapel();
+  };
+
+  const getColumnSearchProps = (dataIndex, nestedPath) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() =>
+            handleSearchTable(selectedKeys, confirm, dataIndex)
+          }
+          style={{ marginBottom: 8, display: "block" }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearchTable(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button type="link" size="small" onClick={() => close()}>
+            Close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />
+    ),
+    onFilter: (value, record) => {
+      if (nestedPath) {
+        const nestedValue = nestedPath
+          .split(".")
+          .reduce((obj, key) => obj?.[key], record);
+        return nestedValue
+          ?.toString()
+          .toLowerCase()
+          .includes(value.toLowerCase());
+      }
+      return record[dataIndex]
+        ?.toString()
+        .toLowerCase()
+        .includes(value.toLowerCase());
+    },
+    filterDropdownProps: {
+      onOpenChange(open) {
+        if (open) setTimeout(() => searchInput.current?.select(), 100);
+      },
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text?.toString() || ""}
+        />
+      ) : (
+        text
+      ),
+  });
+
   const renderColumns = () => [
     {
       title: "ID Mapel",
       dataIndex: "idMapel",
       key: "idMapel",
       align: "center",
+      render: (_, __, index) => index + 1,
     },
     {
       title: "Nama Mapel",
       dataIndex: "name",
       key: "name",
       align: "center",
+      ...getColumnSearchProps("name"),
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: "Sekolah",
+      dataIndex: ["school", "nameSchool"],
+      key: "nameSchool",
+      align: "center",
+      ...getColumnSearchProps("nameSchool", "school.nameSchool"),
+      sorter: (a, b) => a.school.nameSchool.localeCompare(b.school.nameSchool),
     },
     {
       title: "Operasi",
@@ -137,6 +334,33 @@ const Mapel = () => {
     },
   ];
 
+  const renderTable = () => (
+    <Table
+      rowKey="idMapel"
+      dataSource={filteredData}
+      columns={renderColumns()}
+      pagination={{ pageSize: 10 }}
+    />
+  );
+
+  const renderButtons = () => (
+    <Row gutter={[16, 16]} justify="start">
+      <Col>
+        <Button type="primary" onClick={() => setAddMapelModalVisible(true)}>
+          Tambahkan Mapel
+        </Button>
+      </Col>
+      <Col>
+        <Button
+          icon={<UploadOutlined />}
+          onClick={() => setImportModalVisible(true)}
+        >
+          Import File
+        </Button>
+      </Col>
+    </Row>
+  );
+
   return (
     <div className="app-container">
       <TypingCard
@@ -144,26 +368,41 @@ const Mapel = () => {
         source="Di sini, Anda dapat mengelola mapel di sistem."
       />
       <br />
-      <Card
-        title={
-          <Button
-            type="primary"
-            icon={<PlusOutlined />}
-            onClick={handleAddMapel}
+      {loading ? (
+        <Card>
+          <Skeleton active paragraph={{ rows: 10 }} />
+        </Card>
+      ) : (
+        <Card style={{ overflowX: "scroll" }}>
+          {/* Baris untuk tombol dan pencarian */}
+          <Row
+            justify="space-between"
+            align="middle"
+            style={{ marginBottom: 16 }}
           >
-            Tambah Mapel
-          </Button>
-        }
-      >
-        <Table
-          rowKey="idMapel"
-          dataSource={mapel}
-          columns={renderColumns()}
-          pagination={{ pageSize: 10 }}
-        />
-      </Card>
+            {/* Tombol Tambah & Import */}
+            {renderButtons()}
+
+            {/* Kolom Pencarian */}
+            <Col>
+              <Input.Search
+                key="search"
+                placeholder="Cari mapel..."
+                allowClear
+                enterButton
+                onChange={(e) => setSearchQuery(e.target.value)}
+                style={{ width: 300 }}
+              />
+            </Col>
+          </Row>
+
+          {/* Tabel */}
+          {renderTable()}
+        </Card>
+      )}
 
       <AddMapelForm
+        wrappedComponentRef={addMapelFormRef}
         visible={addMapelModalVisible}
         confirmLoading={addMapelModalLoading}
         onCancel={handleCancel}
@@ -171,11 +410,36 @@ const Mapel = () => {
       />
 
       <EditMapelForm
+        wrappedComponentRef={editMapelFormRef}
         currentRowData={currentRowData}
         visible={editMapelModalVisible}
         confirmLoading={editMapelModalLoading}
         onCancel={handleCancel}
+        onOk={handleEditMapelOk}
       />
+
+      <Modal
+        title="Import File"
+        open={importModalVisible}
+        onCancel={() => setImportModalVisible(false)}
+        footer={[
+          <Button key="cancel" onClick={() => setImportModalVisible(false)}>
+            Cancel
+          </Button>,
+          <Button
+            key="upload"
+            type="primary"
+            loading={uploading}
+            onClick={() => {}}
+          >
+            Upload
+          </Button>,
+        ]}
+      >
+        <Upload beforeUpload={() => false} accept=".csv,.xlsx,.xls">
+          <Button>Pilih File</Button>
+        </Upload>
+      </Modal>
     </div>
   );
 };
