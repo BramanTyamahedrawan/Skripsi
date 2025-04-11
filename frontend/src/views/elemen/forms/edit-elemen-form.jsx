eb45acab632c495677614d41899d2da710f8321c
braman tyamahendrawan <brammahendrawan693@gmail.com>
2025-04-12 02:07:06 +0700

    refactor elemen
    Signed-off-by: braman tyamahendrawan <brammahendrawan693@gmail.com>

--- frontend/src/views/elemen/forms/edit-elemen-form.jsx
+++ frontend/src/views/elemen/forms/edit-elemen-form.jsx
@@ -1,14 +1,19 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from "react";
-import { Form, Input, Modal, Select, Row, Col } from "antd";
+import { Form, Input, Modal, Select, Tabs, Row, Col, message } from "antd";
+import { getElemen } from "@/api/elemen";
+import { getSchool } from "@/api/school";
+import { reqUserInfo } from "@/api/user";
 import { getKelas } from "@/api/kelas";
 import { getTahunAjaran } from "@/api/tahun-ajaran";
 import { getSemester } from "@/api/semester";
 import { getMapel } from "@/api/mapel";
-import { getKonsentrasiKeahlian } from "@/api/konsentrasiKeahlian";
+import { getKonsentrasiKeahlianSekolah } from "@/api/konsentrasiKeahlianSekolah";
 
+const { TextArea } = Input;
 const { Option } = Select;
+const { TabPane } = Tabs;
 
 const EditElemenForm = ({
   visible,
@@ -18,101 +23,162 @@ const EditElemenForm = ({
   currentRowData,
 }) => {
   const [form] = Form.useForm();
+  const [elemen, setElemen] = useState([]);
+
+  const [tableLoading, setTableLoading] = useState(false);
+  const [userSchoolId, setUserSchoolId] = useState([]); // State untuk menyimpan ID sekolah user
+  const [schoolList, setSchoolList] = useState([]);
   const [kelasList, setKelasList] = useState([]);
   const [tahunAjaranList, setTahunAjaranList] = useState([]);
   const [semesterList, setSemesterList] = useState([]);
   const [mapelList, setMapelList] = useState([]);
-  const [konsentrasiKeahlianList, setKonsentrasiKeahlianList] = useState([]);
+  const [konsentrasiKeahlianSekolahList, setKonsentrasiKeahlianSekolahList] =
+    useState([]);
+
+  const fetchUserInfo = async () => {
+    try {
+      const response = await reqUserInfo(); // Ambil data user dari API
+      setUserSchoolId(response.data.school_id); // Simpan ID sekolah user ke state
+      console.log("User School ID: ", response.data.school_id);
+    } catch (error) {
+      message.error("Gagal mengambil informasi pengguna");
+    }
+  };
+
+  const fetchSchoolList = async () => {
+    try {
+      const result = await getSchool();
+      if (result.data.statusCode === 200) {
+        setSchoolList(result.data.content);
+      } else {
+        message.error("Gagal mengambil data");
+      }
+    } catch (error) {
+      message.error("Terjadi kesalahan: " + error.message);
+    }
+  };
+
+  const fetchElemen = async () => {
+    setTableLoading(true);
+    try {
+      const result = await getElemen();
+      if (result.data.statusCode === 200) {
+        setElemen(result.data.content);
+      } else {
+        message.error("Gagal mengambil data");
+      }
+    } catch (error) {
+      message.error("Terjadi kesalahan: " + error.message);
+    } finally {
+      setTableLoading(false);
+    }
+  };
 
   const fetchKelasList = async () => {
     try {
       const result = await getKelas();
-      const { content, statusCode } = result.data;
-      if (statusCode === 200) {
-        setKelasList(content);
+      if (result.data.statusCode === 200) {
+        setKelasList(result.data.content);
       } else {
-        console.log("Error: ", result.data.message);
+        message.error("Gagal mengambil data");
       }
     } catch (error) {
-      console.log("Error: ", error);
+      message.error("Terjadi kesalahan: " + error.message);
+    } finally {
+      setTableLoading(false);
     }
   };
 
   const fetchTahunAjaranList = async () => {
     try {
       const result = await getTahunAjaran();
-      const { content, statusCode } = result.data;
-      if (statusCode === 200) {
-        setTahunAjaranList(content);
+      if (result.data.statusCode === 200) {
+        setTahunAjaranList(result.data.content);
       } else {
-        console.log("Error: ", result.data.message);
+        message.error("Gagal mengambil data");
       }
     } catch (error) {
-      console.log("Error: ", error);
+      message.error("Terjadi kesalahan: " + error.message);
+    } finally {
+      setTableLoading(false);
     }
   };
 
   const fetchSemesterList = async () => {
     try {
       const result = await getSemester();
-      const { content, statusCode } = result.data;
-      if (statusCode === 200) {
-        setSemesterList(content);
+      if (result.data.statusCode === 200) {
+        setSemesterList(result.data.content);
       } else {
-        console.log("Error: ", result.data.message);
+        message.error("Gagal mengambil data");
       }
     } catch (error) {
-      console.log("Error: ", error);
+      message.error("Terjadi kesalahan: " + error.message);
+    } finally {
+      setTableLoading(false);
     }
   };
 
   const fetchMapelList = async () => {
     try {
       const result = await getMapel();
-      const { content, statusCode } = result.data;
-      if (statusCode === 200) {
-        setMapelList(content);
+      if (result.data.statusCode === 200) {
+        setMapelList(result.data.content);
       } else {
-        console.log("Error: ", result.data.message);
+        message.error("Gagal mengambil data");
       }
     } catch (error) {
-      console.log("Error: ", error);
+      message.error("Terjadi kesalahan: " + error.message);
+    } finally {
+      setTableLoading(false);
     }
   };
 
-  const fetchKonsentrasiKeahlianList = async () => {
+  const fetchKonsentrasiKeahlianSekolahList = async () => {
     try {
-      const result = await getKonsentrasiKeahlian();
-      const { content, statusCode } = result.data;
-      if (statusCode === 200) {
-        setKonsentrasiKeahlianList(content);
+      const result = await getKonsentrasiKeahlianSekolah();
+      if (result.data.statusCode === 200) {
+        setKonsentrasiKeahlianSekolahList(result.data.content);
       } else {
-        console.log("Error: ", result.data.message);
+        message.error("Gagal mengambil data");
       }
     } catch (error) {
-      console.log("Error: ", error);
+      message.error("Terjadi kesalahan: " + error.message);
+    } finally {
+      setTableLoading(false);
     }
   };
 
   useEffect(() => {
+    fetchUserInfo();
+    fetchSchoolList();
+    fetchElemen();
     fetchKelasList();
     fetchTahunAjaranList();
     fetchSemesterList();
     fetchMapelList();
-    fetchKonsentrasiKeahlianList();
+    fetchKonsentrasiKeahlianSekolahList();
 
     if (currentRowData) {
       form.setFieldsValue({
         idElemen: currentRowData.idElemen,
         namaElemen: currentRowData.namaElemen,
+        idSchool: currentRowData.school?.idSchool,
         idKelas: currentRowData.kelas?.idKelas,
         idTahun: currentRowData.tahunAjaran?.idTahun,
         idSemester: currentRowData.semester?.idSemester,
         idMapel: currentRowData.mapel?.idMapel,
-        id: currentRowData.konsentrasiKeahlian?.id,
+        idKonsentrasiSekolah:
+          currentRowData.konsentrasiKeahlianSekolah?.idKonsentrasiSekolah,
       });
     }
   }, [currentRowData, form]);
+
+  useEffect(() => {
+    if (userSchoolId) {
+      form.setFieldsValue({ idSchool: userSchoolId });
+    }
+  }, [userSchoolId, form]);
 
   const handleSubmit = async () => {
     try {
@@ -125,7 +191,7 @@ const EditElemenForm = ({
 
   return (
     <Modal
-      title="Edit Konsentrasi Keahlian"
+      title="Edit Elemen"
       open={visible}
       onCancel={() => {
         form.resetFields();
@@ -140,11 +206,19 @@ const EditElemenForm = ({
         <Row gutter={16}>
           <Col xs={24} sm={24} md={12}>
             <Form.Item
-              label="ID Elemen:"
-              name="idElemen"
-              rules={[{ required: true, message: "Silahkan isi ID Elemen" }]}
+              label="Sekolah:"
+              name="idSchool"
+              rules={[{ required: true, message: "Silahkan pilih Kelas" }]}
             >
-              <Input placeholder="Masukkan ID Elemen" />
+              <Select defaultValue={userSchoolId} disabled>
+                {schoolList
+                  .filter(({ idSchool }) => idSchool === userSchoolId) // Hanya menampilkan sekolah user
+                  .map(({ idSchool, nameSchool }) => (
+                    <Option key={idSchool} value={idSchool}>
+                      {nameSchool}
+                    </Option>
+                  ))}
+              </Select>
             </Form.Item>
           </Col>
           <Col xs={24} sm={24} md={12}>
@@ -220,16 +294,26 @@ const EditElemenForm = ({
           </Col>
           <Col xs={24} sm={24} md={12}>
             <Form.Item
-              label="Konsentrasi Keahlian:"
+              label="Konsentrasi Keahlian Sekolah:"
               name="id"
-              rules={[{ required: true, message: "Silahkan pilih Mapel" }]}
+              rules={[
+                {
+                  required: true,
+                  message: "Silahkan pilih Konsentrasi Keahlian Sekolah",
+                },
+              ]}
             >
-              <Select placeholder="Pilih Konsetrasi Keahlian">
-                {konsentrasiKeahlianList.map(({ id, konsentrasi }) => (
-                  <Option key={id} value={id}>
-                    {konsentrasi}
-                  </Option>
-                ))}
+              <Select placeholder="Pilih Konsetrasi Keahlian Sekolah">
+                {konsentrasiKeahlianSekolahList.map(
+                  ({ idKonsentrasiSekolah, namaKonsentrasiSekolah }) => (
+                    <Option
+                      key={idKonsentrasiSekolah}
+                      value={idKonsentrasiSekolah}
+                    >
+                      {namaKonsentrasiSekolah}
+                    </Option>
+                  )
+                )}
               </Select>
             </Form.Item>
           </Col>
--- frontend/src/views/elemen/index.jsx
+++ frontend/src/views/elemen/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import {
   Card,
   Button,
@@ -10,16 +10,22 @@ import {
   Col,
   Divider,
   Modal,
+  Input,
+  Space,
 } from "antd";
 import {
   EditOutlined,
   DeleteOutlined,
   UploadOutlined,
+  SearchOutlined,
 } from "@ant-design/icons";
 import { getElemen, deleteElemen, addElemen, editElemen } from "@/api/elemen";
 import TypingCard from "@/components/TypingCard";
 import AddElemenForm from "./forms/add-elemen-form";
 import EditElemenForm from "./forms/edit-elemen-form";
+import { Skeleton } from "antd";
+import Highlighter from "react-highlight-words";
+import { reqUserInfo, getUserById } from "@/api/user";
 
 const Elemen = () => {
   const [elemen, setElemen] = useState([]);
@@ -30,24 +36,68 @@ const Elemen = () => {
   const [importModalVisible, setImportModalVisible] = useState(false);
   const [uploading, setUploading] = useState(false);
   const [currentRowData, setCurrentRowData] = useState({});
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [userIdJson, setUserIdJson] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const searchInput = useRef(null);
 
   const addFormRef = useRef(null);
   const editFormRef = useRef(null);
 
   useEffect(() => {
-    fetchELemen();
+    const initializeData = async () => {
+      const userInfoResponse = await reqUserInfo();
+      const { id: userId } = userInfoResponse.data;
+
+      await getUserInfoJson(userId);
+    };
+
+    initializeData();
   }, []);
 
-  const fetchELemen = async () => {
+  const fetchELemen = useCallback(async () => {
+    setLoading(true);
     try {
       const result = await getElemen();
-      if (result.data.statusCode === 200) {
-        setElemen(result.data.content);
+      const { content, statusCode } = result.data;
+      if (statusCode === 200) {
+        const filteredContent = content.filter(
+          (item) => item.school?.idSchool === userIdJson
+        );
+        setElemen(filteredContent);
       } else {
         message.error("Gagal mengambil data");
       }
     } catch (error) {
       message.error("Terjadi kesalahan: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, [userIdJson]);
+
+  useEffect(() => {
+    if (userIdJson) {
+      fetchELemen();
+    }
+  }, [userIdJson, fetchELemen]);
+
+  const filteredData = elemen.filter((item) => {
+    const query = searchQuery.toLowerCase();
+    return (
+      (item?.namaElemen?.toLowerCase() || "").includes(query) ||
+      (item?.nameSchool?.toLowerCase() || "").includes(query)
+    );
+  });
+
+  const getUserInfoJson = async (userId) => {
+    const result = await getUserById(userId);
+    const { content, statusCode } = result.data;
+    if (statusCode === 200) {
+      setUserIdJson(content[0].school.idSchool); // Ubah dari userId ke schoolId
     }
   };
 
@@ -76,6 +126,31 @@ const Elemen = () => {
     setEditModalVisible(true);
   };
 
+  const handleAddOk = async (values) => {
+    setAddModalLoading(true);
+    try {
+      const updatedValues = {
+        idElemen: null,
+        namaElemen: values.namaElemen,
+        idKonsentrasi: values.id,
+        idKelas: values.idKelas,
+        idTahun: values.idTahun,
+        idSemester: values.idSemester,
+        idMapel: values.idMapel,
+        idSekolah: values.idSchool,
+      };
+      console.log("respon data", updatedValues);
+      await addElemen(updatedValues);
+      setAddModalVisible(false);
+      setAddModalLoading(false);
+      message.success("Berhasil menambahkan");
+      fetchELemen();
+    } catch (error) {
+      setAddModalLoading(false);
+      message.error("Gagal menambahkan: " + error.message);
+    }
+  };
+
   const handleEditOk = async (values) => {
     setEditModalLoading(true);
     try {
@@ -87,6 +162,7 @@ const Elemen = () => {
         idTahun: values.idTahun,
         idSemester: values.idSemester,
         idMapel: values.idMapel,
+        idSekolah: values.idSchool,
       };
 
       console.log("respon data", updatedValues);
@@ -101,34 +177,117 @@ const Elemen = () => {
     }
   };
 
-  const handleAddOk = async (values) => {
-    setAddModalLoading(true);
-    try {
-      const updatedValues = {
-        idElemen: values.idElemen,
-        namaElemen: values.namaElemen,
-        idKonsentrasi: values.id,
-        idKelas: values.idKelas,
-        idTahun: values.idTahun,
-        idSemester: values.idSemester,
-        idMapel: values.idMapel,
-      };
-      console.log("respon data", updatedValues);
-      await addElemen(updatedValues);
-      setAddModalVisible(false);
-      setAddModalLoading(false);
-      message.success("Berhasil menambahkan");
-      fetchELemen();
-    } catch (error) {
-      setAddModalLoading(false);
-      message.error("Gagal menambahkan: " + error.message);
-    }
-  };
-
   const handleCancel = () => {
     setAddModalVisible(false);
     setEditModalVisible(false);
   };
+
+  const handleSearchTable = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+
+  const handleSearch = (keyword) => {
+    setSearchKeyword(keyword);
+    getElemen();
+  };
+
+  const getColumnSearchProps = (dataIndex, nestedPath) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() =>
+            handleSearchTable(selectedKeys, confirm, dataIndex)
+          }
+          style={{ marginBottom: 8, display: "block" }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearchTable(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button type="link" size="small" onClick={() => close()}>
+            Close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />
+    ),
+    onFilter: (value, record) => {
+      if (nestedPath) {
+        const nestedValue = nestedPath
+          .split(".")
+          .reduce((obj, key) => obj?.[key], record);
+        return nestedValue
+          ?.toString()
+          .toLowerCase()
+          .includes(value.toLowerCase());
+      }
+      return record[dataIndex]
+        ?.toString()
+        .toLowerCase()
+        .includes(value.toLowerCase());
+    },
+    filterDropdownProps: {
+      onOpenChange(open) {
+        if (open) setTimeout(() => searchInput.current?.select(), 100);
+      },
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text?.toString() || ""}
+        />
+      ) : (
+        text
+      ),
+  });
 
   const renderColumns = () => [
     {
@@ -143,36 +302,64 @@ const Elemen = () => {
       dataIndex: "namaElemen",
       key: "namaElemen",
       align: "center",
+      ...getColumnSearchProps("namaElemen"),
+      sorter: (a, b) => a.namaElemen.localeCompare(b.namaElemen),
     },
     {
       title: "Mata Pelajaran",
       dataIndex: ["mapel", "name"],
       key: "name",
       align: "center",
+      ...getColumnSearchProps("name", "mapel.name"),
+      sorter: (a, b) => a.mapel.name.localeCompare(b.mapel.name),
     },
     {
       title: "Tahun Ajaran",
       dataIndex: ["tahunAjaran", "tahunAjaran"],
       key: "tahunAjaran",
       align: "center",
+      ...getColumnSearchProps("tahunAjaran", "tahunAjaran.tahunAjaran"),
+      sorter: (a, b) =>
+        a.tahunAjaran.tahunAjaran.localeCompare(b.tahunAjaran.tahunAjaran),
     },
     {
       title: "Semester",
       dataIndex: ["semester", "namaSemester"],
       key: "namaSemester",
       align: "center",
+      ...getColumnSearchProps("namaSemester", "semester.namaSemester"),
+      sorter: (a, b) =>
+        a.semester.namaSemester.localeCompare(b.semester.namaSemester),
     },
     {
       title: "Kelas",
       dataIndex: ["kelas", "namaKelas"],
       key: "namaKelas",
       align: "center",
+      ...getColumnSearchProps("namaKelas", "kelas.namaKelas"),
+      sorter: (a, b) => a.kelas.namaKelas.localeCompare(b.kelas.namaKelas),
     },
     {
-      title: "Konsentrasi Keahlian",
-      dataIndex: ["konsentrasiKeahlian", "konsentrasi"],
-      key: "konsentrasi",
+      title: "Konsentrasi Keahlian Sekolah",
+      dataIndex: ["konsentrasiKeahlianSekolah", "namaKonsentrasiSekolah"],
+      key: "namaKonsentrasiSekolah",
       align: "center",
+      ...getColumnSearchProps(
+        "namaKonsentrasiSekolah",
+        "konsentrasiKeahlianSekolah.namaKonsentrasiSekolah"
+      ),
+      sorter: (a, b) =>
+        a.konsentrasiKeahlianSekolah.namaKonsentrasiSekolah.localeCompare(
+          b.konsentrasiKeahlianSekolah.namaKonsentrasiSekolah
+        ),
+    },
+    {
+      title: "Sekolah",
+      dataIndex: ["school", "nameSchool"],
+      key: "nameSchool",
+      align: "center",
+      ...getColumnSearchProps("nameSchool", "school.nameSchool"),
+      sorter: (a, b) => a.school.nameSchool.localeCompare(b.school.nameSchool),
     },
     {
       title: "Operasi",
@@ -201,8 +388,8 @@ const Elemen = () => {
 
   const renderTable = () => (
     <Table
-      rowKey="id"
-      dataSource={elemen}
+      rowKey="idElemen"
+      dataSource={filteredData}
       columns={renderColumns()}
       pagination={{ pageSize: 10 }}
     />
@@ -233,7 +420,38 @@ const Elemen = () => {
         source="Di sini, Anda dapat mengelola elemen  di sistem."
       />
       <br />
-      <Card title={renderButtons()}>{renderTable()}</Card>
+      {loading ? (
+        <Card>
+          <Skeleton active paragraph={{ rows: 10 }} />
+        </Card>
+      ) : (
+        <Card style={{ overflowX: "scroll" }}>
+          {/* Baris untuk tombol dan pencarian */}
+          <Row
+            justify="space-between"
+            align="middle"
+            style={{ marginBottom: 16 }}
+          >
+            {/* Tombol Tambah & Import */}
+            {renderButtons()}
+
+            {/* Kolom Pencarian */}
+            <Col>
+              <Input.Search
+                key="search"
+                placeholder="Cari Elemen..."
+                allowClear
+                enterButton
+                onChange={(e) => setSearchQuery(e.target.value)}
+                style={{ width: 300 }}
+              />
+            </Col>
+          </Row>
+
+          {/* Tabel */}
+          {renderTable()}
+        </Card>
+      )}
 
       <AddElemenForm
         wrappedComponentRef={addFormRef}
